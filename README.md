d46b623580e942d5f41c1831cd23132c19f22268
mrabit <yuany@mrabit.com>
2022-08-01 10:39:52 +0800

    feat: 新增 UDP 支持

--- README.md
+++ README.md
@@ -2,24 +2,19 @@
 
 ## 简介
 
-通过 [socat](http://www.dest-unreach.org/socat/) 实现 TCP端口转发
+通过 [socat](http://www.dest-unreach.org/socat/) 实现 TCP/UDP 端口转发
 
 ## 使用
 
 ```shell
 # 构建docker镜像
-docker build -t tcp-proxy .
-# 创建配置文件
-mv tcp-proxy.conf /etc
-# 运行容器
-docker run --name proxy -d -v /etc/tcp-proxy.conf:/etc/tcp-proxy.conf --net=host tcp-proxy
+docker-compose up -d
 ```
 
-## tcp-proxy.conf配置
+## tcp-proxy.conf 配置
 
 ```
 #localPort targetHost:targetPort
 1098 127.0.0.1:80
 1099 127.0.0.1:80
 ```
-
--- tcp-proxy.sh
+++ tcp-proxy.sh
@@ -5,8 +5,10 @@ for line in $(cat /etc/tcp-proxy.conf)
 do 
     IFS=' '
     LISTEN=(${line})
-    echo "relay TCP/IP connections on :${LISTEN[0]} to ${LISTEN[1]}" >> /var/log/tcp-proxy.log
     exec socat -d TCP-LISTEN:${LISTEN[0]},fork,reuseaddr TCP:${LISTEN[1]} &
+    echo "relay TCP/IP connections on :${LISTEN[0]} to ${LISTEN[1]}" >> /var/log/tcp-proxy.log
+    exec socat -d UDP4-RECVFROM:${LISTEN[0]},fork,reuseaddr UDP4-SENDTO:${LISTEN[1]} &
+    echo "relay UDP/IP connections on :${LISTEN[0]} to ${LISTEN[1]}" >> /var/log/tcp-proxy.log
 done
 
 tail -f /var/log/tcp-proxy.log
